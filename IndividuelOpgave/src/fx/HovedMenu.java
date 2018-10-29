1339703d925980225a5305f4ca450e42458d5a5d
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2018-10-29 12:52:15 +0100

    v3

--- IndividuelOpgave/src/fx/HovedMenu.java
+++ IndividuelOpgave/src/fx/HovedMenu.java
@@ -17,9 +17,6 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import logic.Properties;
 import logic.PropertiesImpl;
-import logic.Ven;
-import logic.VennerOgBekendte;
-import logic.VennerOgBekendteFactory;
 import logic.PropertiesFactory;
 
 
@@ -39,6 +36,7 @@ public class HovedMenu extends Application {
 
 		Label label0 = new Label("Individuel Opgave");
 		Button btnGem = new Button("Gem");
+		
 		Label label1 = new Label("Key : ");
 		TextField tfKey = new TextField();
 		tfKey.setMaxWidth(200);
@@ -46,36 +44,73 @@ public class HovedMenu extends Application {
 		TextField tfValue = new TextField();
 		tfValue.setMaxWidth(200);
 		btnGem.setPrefSize(100, 20);
-		
+
 		Button btnSlet = new Button("Slet");
 		btnSlet.setPrefSize(100, 20);
-	
+
 		Button btnHent = new Button("Hent");
 		btnHent.setPrefSize(100, 20);
 		
+		
+		Label hentProperty = new Label("");
 		VBox layout0 = new VBox(20);
 		layout0.setAlignment(Pos.CENTER);
-		layout0.getChildren().addAll(label0, label1, tfKey, label2, tfValue, btnGem, btnSlet, btnHent);
+		layout0.getChildren().addAll(label0, label1, tfKey, label2, tfValue, hentProperty, btnGem, btnSlet, btnHent);
 		Hovedmenu = new Scene(layout0, 800, 600);
 		window.setScene(Hovedmenu);
 		window.show();
-		
+
+		btnGem.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				new PropertiesFactory();
+				Properties property = PropertiesFactory.makeProperties();
+				getProperty(tfKey);
+				Properties property = property.hent(tfKey.getProperty());
+				if (ven != null) {
+					hentProperty.setProperty(property.toString());
+				} else
+					hentProperty.setText("Prøv igen");
+
+			}
+		});
+
 		btnHent.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				Properties property = new PropertiesFactory().makeProperties();
-				Property property = property.hent(ind.getProperty());
+				new PropertiesFactory();
+				Properties property = PropertiesFactory.makeProperties();
+				getProperty(tfKey);
+				Properties property = property.hent(tfKey.getProperty());
+				if (ven != null) {
+					hentProperty.setProperty(Property.toString());
+				} else
+					hentProperty.setText("Prøv igen");
+
+			}
+		});
+		
+		btnSlet.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				new PropertiesFactory();
+				Properties property = PropertiesFactory.makeProperties();
+				getProperty(tfKey);
+				Properties property = property.hent(tfKey.getProperty());
 				if (ven != null) {
 					hentPerson.setText(ven.toString());
 				} else
 					hentPerson.setText("Prøv igen");
 
-				// what to do
-				
 			}
 		});
 		
-
-}
+		
+		
+		
+		
+	}
 }
